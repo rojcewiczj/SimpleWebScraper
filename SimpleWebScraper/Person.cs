eb251eaf0a407c3823dfc28ed973f5e4c6487240
John Rojcewicz <rojcewiczj@gmail.com>
2020-10-27 13:37:42 -0500

    Add project files.

---
+++ SimpleWebScraper/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebScraper
+{
+    class Person
+    {
+        string _ssn = "SSN";
+        string _passportData = "DATA";
+        string _driversLicenseNumber = "More DATA";
+        string _firstName = "Avetis";
+        string _lastName = "Ghuk";
+
+        public bool HasProperDocuments
+        {
+            get
+            {
+                return _ssn.Length > 0 && _passportData.Length > 0 && _driversLicenseNumber.Length > 0;
+            }
+        }
+
+       
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (value.Length < 1)
+                {
+                    Console.WriteLine("Input is not accepted");
+                    return;
+                }
+                _firstName = value;
+            }
+        }
+
+
+        public string LastName { get; set; }
+    }
+}
---
+++ SimpleWebScraper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Avatis";
+            person.LastName = "Ghuk";
+
+            
+        }
+    }
+}
